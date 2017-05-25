27f94644192e4326a4921df42a90b7c7fc240eb9
Brityich <britov333@gmail.com>
2017-05-25 19:27:14 +0300

    make some imposition

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,15 +4,15 @@ module.exports = function(grunt) {
         sass: {
             dist: {
                 options: {
-                     sourcemap: false,
-                     compress: false,
-                     yuicompress: false,
-                     style: 'expanded',
-                     lineNumbers: true
+                    sourcemap: false,
+                    compress: false,
+                    yuicompress: false,
+                    style: 'expanded',
+                    lineNumbers: true
                     // browsers: ['last 2 versions', 'ie 9']
                 },
                 files: {
-                     'styles.css' : 'scss/styles.scss'
+                    'styles.css' : 'scss/styles.scss'
                 }
             },
         },
@@ -26,20 +26,65 @@ module.exports = function(grunt) {
                 },
                 options: {
                     watchTask: true,
-                     server: './'
+                    server: './'
                 }
             }
         },
         watch: {
             css: {
-                files: 'scss/styles.scss',
-                tasks: ['sass']
+                files: [
+                    'scss/styles.scss',
+                    'scss/core/*.scss',
+                    'scss/content/*.scss',
+                    'html/*.html',
+                    'html/htmls/*.html',
+                    'html/htmls/views/*.html'
+                ],
+                tasks: ['sass', 'htmlbuild']
+            }
+        },
+        htmlbuild: {
+            dist: {
+                src: 'html/index.html',
+                dest: 'index.html',
+                options: {
+                    beautify: true,
+                    prefix: '//some-cdn',
+                    relative: true,
+                    basePath: false,
+                    styles: {
+                        bundle: [
+                            'styles.css',
+                        ],
+                    },
+                    sections: {
+                        views: 'html/htmls/views/*.html',
+                        header: 'html/htmls/header.html',
+                        footer: 'html/htmls/footer.html',
+                        layout: {
+                            header: 'html/htmls/header.html',
+                            footer: 'html/htmls/footer.html'
+                        }
+                    },
+                    styles: {
+                        bundle: [
+                            'styles.css'
+                        ],
+                        test: 'styles.css'
+                    },
+                    data: {
+                        // Data to pass to templates
+                        version: "0.1.0",
+                        title: "test",
+                    },
+                }
             }
         }
-
-});
-     grunt.loadNpmTasks('grunt-contrib-sass');
-     grunt.loadNpmTasks('grunt-contrib-watch');
-     grunt.loadNpmTasks('grunt-browser-sync');
-     grunt.registerTask('default',['browserSync', 'watch', 'sass']);
+    });
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-browser-sync');
+    grunt.loadNpmTasks('grunt-html-build');
+    grunt.loadNpmTasks('grunt-spritesmith');
+    grunt.registerTask('default',['browserSync', 'watch' ]);
 }
